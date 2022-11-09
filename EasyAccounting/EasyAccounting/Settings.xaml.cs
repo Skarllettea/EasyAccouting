caffe5a816fda70904952c43ff9ee711b98de80f
Skarllettea <dakotaroyz3@mail.ru>
2022-11-09 14:13:32 +0300

    Наработки проекта

---
+++ EasyAccounting/EasyAccounting/Settings.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyAccounting
+{
+    /// <summary>
+    /// Логика взаимодействия для Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string> { "Light", "Dark" };
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.ItemsSource = styles;
+            styleBox.SelectedItem = "Dark";
+        }
+
+        public void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string style = styleBox.SelectedItem as string;
+            var uri = new Uri(style + ".xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            User user = new User();
+            user.Show();
+            this.Close();
+        }
+    }
+}
---
+++ EasyAccounting/EasyAccounting/User.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasyAccounting
+{
+    /// <summary>
+    /// Логика взаимодействия для User.xaml
+    /// </summary>
+    public partial class User : Window
+    {
+        public User()
+        {
+            InitializeComponent();
+
+
+            //lblTime.Text = DateTime.Now.ToString("dd.MM.yyyy, HH:mm:ss");
+        }
+
+        private void tbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (pbPassword.Password.Length == 0)
+            {
+                tblPasswordHint.Visibility = Visibility.Visible;
+            }
+            else
+                tblPasswordHint.Visibility = Visibility.Hidden;
+        }
+
+        private void pbPassword1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (pbPassword1.Password.Length == 0)
+            {
+                tblPasswordHint1.Visibility = Visibility.Visible;
+            }
+            else
+                tblPasswordHint1.Visibility = Visibility.Hidden;
+        }
+
+        private void pbPassword2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (pbPassword2.Password.Length == 0)
+            {
+                tblPasswordHint2.Visibility = Visibility.Visible;
+            }
+            else
+                tblPasswordHint2.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+            this.Close();
+        }
+    }
+}
